df34dab0d878127b65e6149a9df83ee4041b6d5b
Antonín <antonin.kucera@tria-tr.cz>
2026-03-02 10:33:04 +0100

    vytvoril sem jednoduchou hru hadej cislo

--- prvnitest.py
+++ prvnitest.py
@@ -1,1 +1,25 @@
-tahle hra je moc slozita
+print("Hello, World!")
+#I want to create an easy game 
+#I want to create a game where you have to guess a number between 1 and 10
+import random       
+number = random.randint(1, 10)
+guess = 0
+while guess != number:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess < number:
+        print("Too low, try again.")
+    elif guess > number:
+        print("Too high, try again.")
+print("Congratulations! You guessed the number.")
+print("do you want to play again? (yes/no)")
+play_again = input().lower()
+if play_again == "yes":
+    number = random.randint(1, 10)
+    guess = 0
+    while guess != number:
+        guess = int(input("Guess a number between 1 and 10: "))
+        if guess < number:
+            print("Too low, try again.")
+        elif guess > number:
+            print("Too high, try again.")
+    print("Congratulations! You guessed the number.")
