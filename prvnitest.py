930be256f188409d3256fb5fb8f396d2dbff2a81
Antonin Kučera <antonin.kucera@tria-tr.cz>
2026-03-05 14:36:06 +0100

    vytvoril sem prostředí pro tetris

--- prvnitest.py
+++ prvnitest.py
@@ -1,27 +1,25 @@
-print("Hello, World!")
-#I want to create an easy game 
-#I want to create a game where you have to guess a number between 1 and 10
-import random       
-number = random.randint(1, 10)
-guess = 0
-while guess != number:
-    guess = int(input("Guess a number between 1 and 10: "))
-    if guess < number:
-        print("Too low, try again.")
-    elif guess > number:
-        print("Too high, try again.")
-print("Congratulations! You guessed the number.")
-print("do you want to play again? (yes/no)")
-play_again = input().lower()
-if play_again == "yes":
-    number = random.randint(1, 10)
-    guess = 0
-    while guess != number:
-        guess = int(input("Guess a number between 1 and 10: "))
-        if guess < number:
-            print("Too low, try again.")
-        elif guess > number:
-            print("Too high, try again.")
-    print("Congratulations! You guessed the number.")
+import pygame 
+import sys 
 
+pygame.init()
+
+#nastavení okna
+okno = pygame.display.set_mode((800, 700))
+pygame.display.set_caption("Tetris test")
+
+#rizeni casu
+hodiny = pygame.time.Clock()
+
+#hlavní smyčka
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #vyplnění pozadí
+    okno.fill((0, 0, 0))
+    pygame.display.flip
+    hodiny.tick(60)
+    
 
