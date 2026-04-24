c62f327197139703322c60f4cb3598433f7facf7
Antonin Kučera <antonin.kucera@tria-tr.cz>
2026-04-24 21:59:06 +0200

    vytvorena dokumentace a pridan kod

--- 03_chatbot/chatbot.py
+++ 03_chatbot/chatbot.py
@@ -6,3 +6,6 @@ import customtkinter as ctk
 # načtení API klíče z .env souboru
 load_dotenv()
 GROQ_KEY = os.getenv("API_KEY")
+
+# vytvoření klienta pro komunikaci s Groq API
+klient = groq.Groq(api_key=GROQ_KEY)
